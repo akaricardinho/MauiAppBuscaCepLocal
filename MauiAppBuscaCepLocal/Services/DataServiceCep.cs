c268c723464c43ff9f98d020484eb24ccc48eb3b
Usuario <Usuario@CPD03-M05>
2024-09-30 11:21:08 -0300

    DataService Atualizado

--- MauiAppBuscaCepLocal/Services/DataServiceCep.cs
+++ MauiAppBuscaCepLocal/Services/DataServiceCep.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppBuscaCepLocal.Model;
+using Newtonsoft.Json;
+
 
 namespace MauiAppBuscaCepLocal.Services
 {
-    internal class DataServiceCep
+    public class DataServiceCep
     {
+        public static async Task<Endereco?> GetEnderecoByCep(string cep)
+        {
+            Endereco? end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                // 10.0.2.2
+                HttpResponseMessage response = await client.GetAsync("http://10.0.2.2:8000/endereco/by-cep?cep=" + cep);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return end;
+        }
     }
 }
