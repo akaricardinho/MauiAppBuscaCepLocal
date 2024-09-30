7a5f8003d2d0091e3b529f93667b8e026e168838
Usuario <Usuario@CPD03-M05>
2024-09-30 11:17:19 -0300

    Pasta Model atualizada

--- MauiAppBuscaCepLocal/Model/Endereco.cs
+++ MauiAppBuscaCepLocal/Model/Endereco.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MauiAppBuscaCepLocal.Model
 {
-    internal class Endereco
+    public class Endereco
     {
         public int id_logradouro { get; set; }
         public int id_cidade { get; set; }
